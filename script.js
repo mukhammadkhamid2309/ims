5a611aee624df43c0222d4851ef2d3a285ed1e89
mukhammadkhamid2309 <mukhammadkhamid2309@gmail.com>
2024-12-04 15:52:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -14,39 +14,13 @@
 
 // Initialize the HTML5 QR Code Scanner
 const html5QrCode = new Html5Qrcode("reader");
- const resultElement = document.getElementById("result");
-     firebase.database().ref('qr/-ODFh6AmQWOjMf_ew1hl/').on('value', function (snapshot) {
-       var values = snapshot.val();
-          var text = '<table style = "text-align: left;"><tbody>'
-        if(values.tanggal){
-           text = text + '<tr><td> Tanggal </td> <td> : </td> <td>'+ values.tanggal+ '</td></tr>';
-        }if(values.vessel){
-        text = text + '<tr><td> Kapal </td> <td> : </td> <td>'+ values.vessel+ '</td></tr>';
-        }if(values.suplier){
-        text = text + '<tr><td> Suplier </td> <td> : </td> <td>'+ values.suplier+ '</td></tr>';
-        }if(values.material){
-        text = text + '<tr><td> Material </td> <td> : </td> <td>'+ values.material+ '</td></tr>';
-        }if(values.material_number){
-        text = text + '<tr><td> Material Number </td> <td> : </td> <td>'+ values.material_number+ '</td></tr>';
-        }if(values.gudang){
-        text = text + '<tr><td> Gudang </td> <td> : </td> <td>'+ values.gudang+ '</td></tr>';
-        }if(values.quantum){
-        text = text + '<tr><td> Quantity </td> <td> : </td> <td>'+ values.quantum+ ' '+ values.satuan+ '</td></tr>';
-        }if(values.catatan){
-        text = text + '<tr><td> Gudang </td> <td> : </td> <td>'+ values.catatan+ '</td></tr>';
-        }
-        text = text + '</tbody></table>';
-        
-        resultElement.innerHTML = text; 
-      });
-
 // Function to handle success scanning
 function onScanSuccess(decodedText, decodedResult) {
     // Show the scanned result
     const resultElement = document.getElementById("result");
      firebase.database().ref('qr/'+decodedText+'/').on('value', function (snapshot) {
        var values = snapshot.val();
-          var text = '<table style = "text-align: left;"><tbody>'
+          var text = '<table style = "text-align: left; font-size: 14px;"><tbody>'
         if(values.tanggal){
            text = text + '<tr><td> Tanggal </td> <td> : </td> <td>'+ values.tanggal+ '</td></tr>';
         }if(values.vessel){
