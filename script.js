4aef9bd12548b04c30f9ffa408416ebfd1fa496a
mukhammadkhamid2309 <mukhammadkhamid2309@gmail.com>
2024-12-04 15:08:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -45,7 +45,27 @@ function onScanSuccess(decodedText, decodedResult) {
     const resultElement = document.getElementById("result");
      firebase.database().ref(decodedText+'/').on('value', function (snapshot) {
        var values = snapshot.val();
-        resultElement.innerHTML = `Material : <strong>${values.material}</strong>`; 
+          var text = '<table class = "info"><tbody>'
+        if(values.tanggal){
+           text = text + '<tr><td> Tanggal </td> <td> : </td> <td>'+ values.tanggal+ '</td></tr>';
+        }if(values.vessel){
+        text = text + '<tr><td> Kapal </td> <td> : </td> <td>'+ values.vessel+ '</td></tr>';
+        }if(values.suplier){
+        text = text + '<tr><td> Suplier </td> <td> : </td> <td>'+ values.suplier+ '</td></tr>';
+        }if(values.material){
+        text = text + '<tr><td> Material </td> <td> : </td> <td>'+ values.material+ '</td></tr>';
+        }if(values.material_number){
+        text = text + '<tr><td> Material Number </td> <td> : </td> <td>'+ values.material_number+ '</td></tr>';
+        }if(values.gudang){
+        text = text + '<tr><td> Gudang </td> <td> : </td> <td>'+ values.gudang+ '</td></tr>';
+        }if(values.quantum){
+        text = text + '<tr><td> Quantity </td> <td> : </td> <td>'+ values.quantum+ ' '+ values.satuan+ '</td></tr>';
+        }if(values.catatan){
+        text = text + '<tr><td> Gudang </td> <td> : </td> <td>'+ values.catatan+ '</td></tr>';
+        }
+        text = text + '</tbody></table>';
+        
+        resultElement.innerHTML = text; 
       });
 
 
